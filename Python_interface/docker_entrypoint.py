d72d29b6c1103d96bf60cd74dd5b4e1a69c42143
nolan1999 <arthur.jaques@hispeed.ch>
2024-03-19 00:45:19 +0100

    Output h5 when input h5

--- Python_interface/docker_entrypoint.py
+++ Python_interface/docker_entrypoint.py
@@ -8,18 +8,57 @@ import time
 import warnings
 import yaml
 from cerberus import Validator
+from typing import Literal, Optional
 import drift_optimization_functions_2d, drift_optimization_functions_3d, segment_dataset
 
 
-def run(param: dict, localizations: np.ndarray, output_path: str) -> np.ndarray:
-    """Estimate drift from list of localizations.
-    Saves the estimated drift in a .csv file and plots it in a .png figure.
+def load_param(param_path: str) -> dict:
+    """Load parameters from file."""
+    param = yaml.safe_load(open(param_path, "r"))
+    schema = {
+        "dataset_dimension": {"type": "integer", "min": 2, "max": 3},
+        "segmentation_method": {"type": "string", "regex": "^(time|locs|frames)$"},
+        "segmentation_parameter": {"type": "integer", "min": 1},
+        "max_drift_nm": {"type": "float", "min": 0},
+        "initial_gaussian_scale_nm": {"type": "float", "min": 0},
+    }
+    v = Validator(require_all=True)
+    if not v.validate(param, schema):
+        raise ValueError(v.errors)
+    return param
 
-    :param param: algorithm parameters
-    :param localizations: 3d localizations
-    :param output_path: target location of the output files
-    """
-    # segment
+
+def load_localizations(
+    localizations_path: str,
+) -> tuple[np.ndarray, Optional[tuple[int]]]:
+    """Load localizations from file (csv or DECODE h5)."""
+    file_ext = os.path.splitext(localizations_path)[1]
+    if file_ext == ".csv":  # Comet format
+        data = pd.read_csv(localizations_path).head(150000)
+        localizations = np.zeros((len(data["frame"]), 4))
+        for i, label in enumerate(["x [nm]", "y [nm]", "z [nm]", "frame"]):
+            localizations[:, i] = np.asarray(data[label])
+        px_size = None
+    elif file_ext == ".h5":  # DECODE format
+        with h5py.File(localizations_path, "r") as h5:
+            data = {k: v for k, v in h5["data"].items() if v.shape is not None}
+            data.update({k: None for k, v in h5["data"].items() if v.shape is None})
+            meta_data = dict(h5["meta"].attrs)
+            localizations = np.concatenate(
+                (data["xyz"], np.expand_dims(data["frame_ix"], 1)), axis=1
+            )
+            if meta_data["xy_unit"] == "px":
+                px_size = meta_data["px_size"]
+                localizations[:, :2] = localizations[:, :2] * np.array(px_size)
+    else:
+        raise ValueError(
+            f"Localizations file of type '{file_ext}' of file '{localizations_path}' not supported; please provide 'csv' or 'h5'."
+        )
+    return localizations, px_size
+
+
+def get_segmentation(param: dict, localizations: np.ndarray) -> tuple[np.ndarray, int]:
+    """Segment localizations."""
     segmentation_method = param["segmentation_method"]
     segmentation_parameter = param["segmentation_parameter"]
     if segmentation_method == "time":
@@ -36,13 +75,14 @@ def run(param: dict, localizations: np.ndarray, output_path: str) -> np.ndarray:
         )
     else:
         raise ValueError(f"segmentation method {segmentation_method} not recognized")
-    print(f"segmentation into {n_segments} segments done")
+    return localizations, n_segments
 
-    # estimate drift
+
+def get_drift(param: dict, n_segments: int, localizations: np.ndarray) -> np.ndarray:
+    """Estimate drift."""
     max_drift_nm = float(param["max_drift_nm"])
     dataset_dimension = param["dataset_dimension"]
     initial_gaussian_scale_nm = float(param["initial_gaussian_scale_nm"])
-    t = time.time()
     warnings.filterwarnings("ignore")
     if dataset_dimension == 3:
         drift = drift_optimization_functions_3d.optimize_3d_chunked(
@@ -60,34 +100,13 @@ def run(param: dict, localizations: np.ndarray, output_path: str) -> np.ndarray:
             sigma_nm=initial_gaussian_scale_nm,
         )
         drift = drift.reshape(n_segments, 2)
-    print(f"drift estimation done in {np.round(time.time()-t)}s")
-
-    # plot drift
-    frames = np.unique(localizations[:, -1])
-    n_frames = len(frames)
-    fig_drift = plot_drift(drift, n_segments, n_frames, dataset_dimension)
-    fig_drift.savefig(f"{output_path}/drift_estimate.png")
-
-    # save drift
-    frames = np.arange(n_segments)
-    result = np.zeros((len(drift[:, 0]), dataset_dimension + 1))
-    result[:, 0] = frames
-    for i in range(dataset_dimension):
-        result[:, i + 1] = drift[:, i]
-    header = "frame,x_nm,y_nm" + (",z_nm" if dataset_dimension == 3 else "") + "\n"
-    file_contents = (
-        header
-        + str(result.tolist())
-        .replace(" [", "")
-        .replace("[", "")
-        .replace("],", "\n")
-        .replace("]", "")
-    ).encode()
-    with open(f"{output_path}/drift_estimate.csv", "wb+") as f:
-        f.write(file_contents)
+    return drift
 
 
-def plot_drift(drift, n_segments, n_frames, dataset_dimension):
+def plot_drift(
+    drift: np.ndarray, n_segments: int, n_frames: int, dataset_dimension: Literal[2, 3]
+) -> plt.Figure:
+    """Plot drift estimate over frames."""
     fig, ax = plt.subplots()
     ax.plot(np.arange(n_segments) / n_segments * n_frames, drift[:, 0])
     ax.plot(np.arange(n_segments) / n_segments * n_frames, drift[:, 1])
@@ -102,56 +121,30 @@ def plot_drift(drift, n_segments, n_frames, dataset_dimension):
     return fig
 
 
-def _load_param(param_path: str) -> dict:
-    param = yaml.safe_load(open(param_path, "r"))
-    # validate
-    schema = {
-        "dataset_dimension": {"type": "integer", "min": 2, "max": 3},
-        "segmentation_method": {"type": "string", "regex": "^(time|locs|frames)$"},
-        "segmentation_parameter": {"type": "integer", "min": 1},
-        "max_drift_nm": {"type": "float", "min": 0},
-        "initial_gaussian_scale_nm": {"type": "float", "min": 0},
-    }
-    v = Validator(require_all=True)
-    if not v.validate(param, schema):
-        raise ValueError(v.errors)
-    print("validated parameters:", param)
-    return param
-
-
-def _load_localizations(localizations_path: str) -> np.ndarray:
-    file_ext = os.path.splitext(localizations_path)[1]
-
-    if file_ext == ".csv":  # Comet format
-        data = pd.read_csv(localizations_path).head(150000)
-        localizations = np.zeros((len(data["frame"]), 4))
-        for i, label in enumerate(["x [nm]", "y [nm]", "z [nm]", "frame"]):
-            localizations[:, i] = np.asarray(data[label])
-
-    elif file_ext == ".h5":  # DECODE format
-        with h5py.File(localizations_path, "r") as h5:
-            data = {k: v for k, v in h5["data"].items() if v.shape is not None}
-            data.update({k: None for k, v in h5["data"].items() if v.shape is None})
-            meta_data = dict(h5["meta"].attrs)
-            localizations = np.concatenate(
-                (data["xyz"], np.expand_dims(data["frame_ix"], 1)), axis=1
-            )
-            if meta_data["xy_unit"] == "px":
-                localizations[:, :2] = localizations[:, :2] * np.array(
-                    meta_data["px_size"]
-                )
-
+def save_drift(
+    dataset_dimension: Literal[2, 3],
+    n_segments: int,
+    drift: np.ndarray,
+    px_size: Optional[tuple[int]],
+    output_path: str,
+):
+    """Save drift estimate to file (csv or h5)."""
+    frames = np.arange(n_segments)  # frames
+    file_ext = os.path.splitext(output_path)[1]
+    if file_ext == ".csv":
+        data = pd.DataFrame({"frame": frames, "x_nm": drift[:, 0], "y_nm": drift[:, 1]})
+        if dataset_dimension == 3:
+            data["z_nm"] = drift[:, 2]
+        data.to_csv(output_path, index=False)
+    elif file_ext == ".h5":
+        with h5py.File(output_path, "w") as h5:
+            data = h5.create_group("data")
+            data.create_dataset("frame_ix", data=frames)
+            data.create_dataset("xyz_nm", data=drift)
+            meta = h5.create_group("meta")
+            meta.attrs.update({"px_size": px_size})
     else:
-        raise ValueError(
-            f"Localizations file of type '{file_ext}' not supported; please provide 'csv' or 'h5'."
-        )
-
-    frames = np.unique(localizations[:, -1])
-    n_frames = len(frames)
-    print(
-        f"{localizations_path} import successful, {len(localizations[:, 0])} localizations, {n_frames} frames"
-    )
-    return localizations
+        raise ValueError(f"Output file type must be '.csv' or '.h5', not '{file_ext}'")
 
 
 if __name__ == "__main__":
@@ -161,9 +154,22 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output_path")
     args = parser.parse_args()
 
-    param = _load_param(args.param_path)
-    localizations = _load_localizations(args.localizations_path)
-    print("doing something")
+    os.makedirs(args.output_path, exist_ok=True)  # prepare output directory
+    param = load_param(args.param_path)
+    print("Read parameters:", param)
+    localizations, px_size = load_localizations(args.localizations_path)
+    n_locs, n_frames = localizations.shape[0], len(np.unique(localizations[:, -1]))
+    print(f"Loaded {n_locs} localizations from {n_frames} frames")
 
-    os.makedirs(args.output_path, exist_ok=True)
-    run(param, localizations, args.output_path)
+    localizations, n_segments = get_segmentation(param, localizations)
+    print(f"Segmentation into {n_segments} segments done")
+    time_start = time.time()
+    drift = get_drift(param, n_segments, localizations)
+    print(f"Drift estimation done in {time.time() - time_start:.2f} seconds")
+
+    fig_drift = plot_drift(drift, n_segments, n_frames, param["dataset_dimension"])
+    fig_drift.savefig(f"{args.output_path}/drift.png")
+    output_path = (
+        f"{args.output_path}/drift{os.path.splitext(args.localizations_path)[1]}"
+    )
+    save_drift(param["dataset_dimension"], n_segments, drift, px_size, output_path)
