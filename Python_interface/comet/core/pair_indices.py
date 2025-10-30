cfa764fc1af74bf27a35307c875b6a191cc999db
LREIN663 <lrein@live.de>
2025-10-30 10:50:03 +0100

    added legacy code using the histogram approach for finding pairs, just to have it close by in case needed for future testing

--- Python_interface/comet/core/pair_indices.py
+++ Python_interface/comet/core/pair_indices.py
@@ -87,3 +87,56 @@ def pair_indices_kdtree_full_to_file_recursion(coordinates, distance, filename=N
         )
 
 
+def pair_indices_lex_floor_asymmetric(coordinates, distance):
+    for i in range(len(coordinates[0])):
+        coordinates[:, i] -= np.min(coordinates[:, i])
+    coordinates = np.array(np.floor(coordinates / distance), dtype=int)
+    coordinates = np.array(list(map(tuple, coordinates)))
+
+    sort_indices = np.lexsort(coordinates.T)
+
+    if coordinates.shape[1] == 2:
+        tmp = coordinates[:, 0].copy()
+        coordinates[:, 0] = coordinates[:, 1]
+        coordinates[:, 1] = tmp
+    else:
+        tmp = coordinates[:, 0].copy()
+        coordinates[:, 0] = coordinates[:, 1]
+        coordinates[:, 1] = tmp
+        tmp = coordinates[:, 0].copy()
+        coordinates[:, 0] = coordinates[:, 2]
+        coordinates[:, 2] = tmp
+        tmp = coordinates[:, 2].copy()
+        coordinates[:, 2] = coordinates[:, 1]
+        coordinates[:, 1] = tmp
+    # get the unique tuples and their counts
+    unique_tuples, counts = np.unique(coordinates[sort_indices], axis=0, return_counts=True)
+    # get the indices of the similar tuples
+    similar_indices = np.split(sort_indices, np.cumsum(counts[:-1]))
+    idx_i = []
+    idx_j = []
+    pair_idc_estimate = 0
+    for i in range(len(similar_indices)):
+        n_elements = len(similar_indices[i])
+        pair_idc_estimate += 0.5 * n_elements * (n_elements + 1)
+    if pair_idc_estimate > 5E8:  # default 5E8
+        print(pair_idc_estimate)
+        print("memory Error")
+        #raise MemoryError(f"To many Pair indices for RAM ({pair_idc_estimate} pairs)")
+        return [], [], False
+    else:
+        try:
+            for i in range(len(similar_indices)):
+                if len(similar_indices[i]) > 1:
+                    idc = similar_indices[i]
+                    for j in range(len(idc) - 1):
+                        tmp_n_entries = (len(idc) - j) - 1
+                        idx_i.append(np.repeat(np.int32(idc[j]), tmp_n_entries))
+                        idx_j.append(idc[np.arange(tmp_n_entries, dtype=np.int32) + (j + 1)])
+            return np.ascontiguousarray(np.concatenate(idx_i).ravel(), dtype=np.int32), \
+                   np.ascontiguousarray(np.concatenate(idx_j).ravel(), dtype=np.int32), True
+        except Exception as e:
+            print(e)
+            return [], [], False
+
+
